f3b35b48d9e692dd90ad15a3431612f9e0557734
yaseerkhan <yaseerkhan44@gmail.com>
2022-08-15 20:35:44 +0530

    ..

--- get-quote.py
+++ get-quote.py
@@ -12,9 +12,9 @@ def primary():
   last = 13
   rnd1 = random.randint(0, last)
   rnd2 = random.randint(0, last)
-  rnd4 = random.randint(0, last)
+  rnd3 = random.randint(0, last)
 
-  print(quotes[rnd1], quotes[rnd2], quotes[rnd4].strip())
+  print(quotes[rnd1], quotes[rnd2], quotes[rnd3].strip())
 
 
 if __name__== "__main__":
